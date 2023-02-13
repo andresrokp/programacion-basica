fdf6b5b39bc46eac438e3be4aad5ef1998c37266
andresropk <afrokap@gmail.com>
2023-02-13 11:06:29 -0500

    refactor: change score logic
    lives state variables
    
    crate lives display a span
    
    set span when attack
    
    final prompt telling the winner

--- MOKEPON/script.js
+++ MOKEPON/script.js
@@ -8,6 +8,9 @@ const mascots = document.getElementsByName("mascot"); // brings all the elements
 const battleSetup = document.getElementById("battle-setup");
 const playerMascotDiv = document.getElementById("player-mascot");
 const computerMascotDiv = document.getElementById("computer-mascot");
+let playerLivesSpan = null;
+let computerLivesSpan = null;
+
 const fireAttackButton = document.getElementById("fire-attack");
 const waterAttackButton = document.getElementById("water-attack");
 const earthAttackButton = document.getElementById("earth-attack");
@@ -18,6 +21,10 @@ const battleMessages = document.getElementById("battle-messages");
 // get the retry button element
 const retryButton = document.getElementById("retry-button");
 const resetButton = document.getElementById("reset");
+
+//state variables
+let playerMascotLives = 3
+let computerMascotLives = 3
 
 // select mascot button click event listener
 selectMascotButton.addEventListener("click", function() {
@@ -46,9 +53,11 @@ selectMascotButton.addEventListener("click", function() {
     mascotSelection.style.display = "none";
     battleSetup.style.display = "block";
 
-    // update the player and computer mascot divs
-    playerMascotDiv.innerHTML = `<strong>YOU</strong> : ${selectedMascot} (Lives: 3)`;
-    computerMascotDiv.innerHTML = `<strong>PC</strong>: ${computerMascot} (Lives: 3)`;
+    // update the player and create counter span
+    playerMascotDiv.innerHTML = `<strong>YOU</strong>: ${selectedMascot} - Lives: <span id="player-lives">3</span>`;
+    playerLivesSpan = document.getElementById("player-lives");
+    computerMascotDiv.innerHTML = `<strong>PC</strong>: ${computerMascot} - Lives: <span id="computer-lives">3</span>`;
+    computerLivesSpan = document.getElementById("computer-lives")
 });
 
 // attack button click event listeners
@@ -64,56 +73,57 @@ earthAttackButton.addEventListener("click", function() {
 
 // perform attack function
 function performAttack(playerAttack) {
-// select a random computer attack
-const computerAttacks = ["fire", "water", "earth"];
-const computerAttack = computerAttacks[Math.floor(Math.random() * computerAttacks.length)];
+    // select a random computer attack
+    const computerAttacks = ["fire", "water", "earth"];
+    const computerAttack = computerAttacks[Math.floor(Math.random() * computerAttacks.length)];
 
-// determine the winner of the attack
-let result;
-if (playerAttack === computerAttack) {
-    result = "draw";
-} else if (
-    (playerAttack === "fire" && computerAttack === "water") ||
-    (playerAttack === "water" && computerAttack === "earth") ||
-    (playerAttack === "earth" && computerAttack === "fire")
-) {
-    result = "win";
-} else {
-    result = "lose";
-}
+    // determine the winner of the attack
+    let result;
+    if (playerAttack === computerAttack) {
+        result = "draw";
+    } else if (
+        (playerAttack === "fire" && computerAttack === "water") ||
+        (playerAttack === "water" && computerAttack === "earth") ||
+        (playerAttack === "earth" && computerAttack === "fire"))
+    {
+        result = "win";
+    } else {
+        result = "lose";
+    }
 
-// update the lives of the player and computer mascots
-let playerMascotLives = parseInt(playerMascotDiv.textContent.split(" ")[3][0]); //akap
-let computerMascotLives = parseInt(computerMascotDiv.textContent.split(" ")[3][0]); //akap
-if (result === "win") {
-    computerMascotLives--; // akap
-    computerMascotDiv.innerHTML = `💧 ${computerMascotDiv.textContent.split(" ")[1]} (Lives: ${computerMascotLives})`; //akap
-} else if (result === "lose") {
-    playerMascotLives--; // akap
-    playerMascotDiv.innerHTML = `🔥 ${playerMascotDiv.textContent.split(" ")[1]} (Lives: ${playerMascotLives})`; //akap
-}
+    // update the lives of the player and computer mascots // AKAP
+    if (result === "win") {
+        computerMascotLives--;
+        computerLivesSpan.innerHTML = `${computerMascotLives}`;
+    } else if (result === "lose") {
+        playerMascotLives--;
+        playerLivesSpan.innerHTML = `${playerMascotLives}`;
+    }
 
-// add a message to the battle messages div
-let message;
-if (result === "win") {
-    message = `🏆 You won! You used ${playerAttack} and the computer used ${computerAttack}`;
-} else if (result === "lose") {
-    message = `😞 You lost! You used ${playerAttack} and the computer used ${computerAttack}`;
-} else {
-    message = `🤝 It's a draw! You both used ${playerAttack}`;
-}
-battleMessages.innerHTML += `<p>${message}</p>`;
+    // add a message to the battle messages div
+    let message;
+    if (result === "win") {
+        message = `🏆 You won! You used ${playerAttack} and the computer used ${computerAttack}`;
+    } else if (result === "lose") {
+        message = `😞 You lost! You used ${playerAttack} and the computer used ${computerAttack}`;
+    } else {
+        message = `🤝 It's a draw! You both used ${playerAttack}`;
+    }
+    battleMessages.innerHTML += `<p>${message}</p>`;
+    
+    // check if the game is over
+    if (playerMascotLives  === 0 || computerMascotLives === 0) { //akap
+        message = playerMascotLives ? '🥳🎉 PLAYER' : '😔⚰️ PC'
+        battleMessages.innerHTML += `<p>${message} WON !!</p>`;
 
-// check if the game is over
-if (playerMascotLives  === 0 || computerMascotLives === 0) { //akap
-    // disable the attack buttons
-    fireAttackButton.disabled = true;
-    waterAttackButton.disabled = true;
-    earthAttackButton.disabled = true;
+        // disable the attack buttons
+        fireAttackButton.disabled = true;
+        waterAttackButton.disabled = true;
+        earthAttackButton.disabled = true;
 
-    // show the retry button
-    retryButton.style.display = "block";
-}
+        // show the retry button
+        retryButton.style.display = "block";
+    }
 }
 
 // reset button click event listener
